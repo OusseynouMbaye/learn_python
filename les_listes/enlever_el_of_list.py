8f9c840e30d686f3048785449461c3760e60252c
Ousseynou <ousseynou.mbaye@dev.iclic.ca>
2021-09-20 12:11:37 -0400

    list:methode pop and join

---
+++ les_listes/enlever_el_of_list.py
@@ -0,0 +1,12 @@
+employes = ["Pier-Luc","Joe","Michel","Ouz","Alex"]
+# print(employes)
+#methode pop:supprimer le dernier element si on lui donne pas d'indice
+# ele = employes.pop()
+# print(ele)
+# employes.clear()
+# print(employes)
+##################################
+#methode join
+liste = ["Mon","nom","est","Ouzinem"]
+resultat = ("\t").join(liste)
+print(resultat)
