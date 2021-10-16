3b55db6e02639274ddd69c52ff2f8d05418f49d9
Ousseynou Mbaye <ouzinem@hotmail.com>
2021-10-16 12:33:57 -0400

    learn list with gary

---
+++ python_with_Gary/learn_list/listes.py
@@ -0,0 +1,13 @@
+# methode append
+liste = [1,2,3,4]
+liste.append(5)
+print(liste)
+# stars = [1,5,4,2,4]
+# # methode 1
+# # somme  = 0
+# # for i in (stars):
+# #     somme+= i
+# # resultats = somme/len(stars)
+# # methode 2
+# resultats = sum(stars) /len(stars)
+# print(resultats)
---
+++ python_with_Gary/learn_list/python_list_methods.py
@@ -0,0 +1,16 @@
+animals = ["dog", "cat", "mokey"]
+print(animals.index("cat"))
+
+# method append
+animals.append("sheep")
+print(animals)
+another_animals = ["pig", "cow"]
+# add list in list
+animals.append(another_animals)
+print(animals)
+
+# methods extend :add animal in the first list
+
+animals_extend = ["horse","birds"]
+animals.extend(animals_extend)
+print(animals)
