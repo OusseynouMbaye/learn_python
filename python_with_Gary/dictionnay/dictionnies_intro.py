f4ee572465c4cb74dd1a09c88e9cdd3206707321
Ousseynou Mbaye <ouzinem@hotmail.com>
2021-10-21 22:11:37 -0400

    disctionnaries add new

--- python_with_Gary/dictionnay/dictionnies_intro.py
+++ python_with_Gary/dictionnay/dictionnies_intro.py
@@ -1,7 +1,23 @@
 user = {"firt_name": "Ouzinem",
-        "address": "St-Georges", "favorite sport": "swim"}
+        "address": "St-Georges", 
+        "favorite_sport": {1:"swim",2: "cycling"},
+         "person": {1: "moi", 2: "toi"}}
 
-print(user["favorite sport"])
-print(user.values())
-print(user.keys())
-# print(user)
+print(user["favorite_sport"].values())
+# print(user["favorite_sport"])
+# print(user.values())
+# print(user.keys())
+# user["taille"] = 180
+# user["favorite_sport"] = "Basket"
+# print(user.fromkeys())
+# Dictionary Methods
+marks = {}.fromkeys(['Math', 'English', 'Science'], 0)
+
+# Output: {'English': 0, 'Math': 0, 'Science': 0}
+# print(marks)
+
+# for item in marks.items():
+#     print(item)
+
+# Output: ['English', 'Math', 'Science']
+# print(list(sorted(marks.keys())))
