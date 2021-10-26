2a27f4fdfb17816091da0be2c74c0f8fa2d78f47
Ousseynou Mbaye <ouzinem@hotmail.com>
2021-10-25 20:28:36 -0400

    revision list

---
+++ python_with_Gary/learn_list/revision-list.py
@@ -0,0 +1,13 @@
+letters = ["a","b","c","d","e","f","g"]
+letters.append("h")
+print(letters)
+letters.pop()
+print(letters)
+letters.pop(2)
+print(letters)
+letters.remove("e")
+print(letters)
+letters.reverse()
+print(letters)
+letter_tri= sorted(letters)  #letters.sort()
+print(letter_tri)
