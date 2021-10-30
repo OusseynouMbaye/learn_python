fd74801e48fb955dbc068655835a4d7631d28ed9
Ousseynou Mbaye <ouzinem@hotmail.com>
2021-10-29 22:39:38 -0400

    learn nested-data
    We learn list in nested list
    We learn dict in nested dict
    we mix dict in nested list

--- python_with_Gary/learn_loop/nested-data.py
+++ python_with_Gary/learn_loop/nested-data.py
@@ -1,19 +1,41 @@
-user_groups = [
-    [
-        {'name': 'Person A', 'isStudent': True},
-        {'name': 'Person B', 'isStudent': True}
-    ],
-    [
-        {'name': 'Person C', 'isStudent': False},
-        {'name': 'Person D', 'isStudent': False}
-    ]
+# nested data with list
+letters = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+]
+# 1 : change a value : we change e for p
+# letters[1][1] = "p"  c
+# print(letters[1][1])  # e
+# 2 : add a new letter in dict : use .append()
+letters[1].append("z")
+# print(letters)
+# 3 : add a new list in the list
+letters.append(["k", "t", "w"])
+# print(letters)
+# for letter in letters:
+#     print(letter)
+
+# 2 nested data with dict
+users = {
+    "user-one": {"id": 1, "name": "Ouzi"},
+    "user-two": {"id": 2, "name": "Sofia"},
+    "user-three": {"id": 3, "name": "Amina"},
+}
+# add a new user in th dict
+users["user-four"] = {"id": 4, "name": "Ali"}
+
+# print(users["user-four"])
+# modify user or add user
+users["user-one"]["name"] = "Aida"
+# print(users)
+
+users2 = [
+    {"id": 1, "name": "Oussou"},
+    {"id": 2, "name": "Zach"},
+    {"id": 3, "name": "Max"}
 ]
 
-for user_group in user_groups:
-    # print(user_group)
-    for user in user_group:
-        # print(user)
-        if user["isStudent"]:
-            print(f'{user["name"]} is student')
-        else:
-            print(f'{user["name"]} is not student')
+# add a new user
+users2.append({"id": 4, "name": "dakar"})
+print(users2)
