f28bd8a3a739770f85b450e874708c484d4e20bb
Ousseynou Mbaye <ouzinem@hotmail.com>
2021-11-17 21:02:10 -0500

    Learn gestion date in python

--- learn_date/gestion-date.py
+++ learn_date/gestion-date.py
@@ -4,7 +4,6 @@ date1 = date(year=2021, month=11, day=13)
 # print(date1)
 time1 = time(hour=13, minute=45, second=12)
 
-quit()
 date_today = date.today()
 time_now = datetime.now()
 print(time_now)
