ef129aa21e84461b5b9e2ac7fa5e1bb6ac6defb4
Ouzinem <ouzinem@hotmail.com>
2021-12-12 14:14:08 -0500

    use another folder2

--- tuples/tuplesIntro.py
+++ tuples/tuplesIntro.py
@@ -1,10 +1,14 @@
-a = (2,5)
+# b = (2, 5, 3)
+
+
 # print(type(a))
 
 def average(a):
-    resultats = sum(a)
+    resultats = sum(a)/len(a)
     return resultats
 
+
+# print(average(b))
 # for name in ("ouz","ali"):
 #     print(name)
 # print(average(1, 3))
