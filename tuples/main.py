4c2ce6024ea4500860f121f1243e189559a69326
Ouzinem <ouzinem@hotmail.com>
2021-12-12 14:13:52 -0500

    use another folder

--- tuples/main.py
+++ tuples/main.py
@@ -1,7 +1,10 @@
 from tuplesIntro import average
+
 num = 4
 num2 = 6
 list_num = [1, 2, 3, 4]
+tupl_num = (1, 2, 3, 4)
 resultat = sum(list_num)
 # print(resultat)
-print(average())
+# print(average(list_num))
+print(average(tupl_num))
