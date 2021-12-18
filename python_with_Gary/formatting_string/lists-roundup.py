db8be22185d578943b1d5202e2fc322181a6785d
Ouzinem <ouzinem@hotmail.com>
2021-12-17 21:38:33 -0500

    learn list roundup
    function zip()

---
+++ python_with_Gary/formatting_string/lists-roundup.py
@@ -0,0 +1,13 @@
+concatenate = [4, 5, 6] + [1, 2, 3]
+
+my_lists = ["Ouzin", "Gary", "Mbaye"] * 3
+
+zipped_list = zip([1, 2, 3], ["Ouzin", "Gary", "Mbaye"])
+# print(list(zipped_list))
+
+# for v1, v2 in zipped_list:
+#     print(v1, '', v2)
+
+# min
+print(min([12, 89, 36, 44]))
+print(max([12, 89, 36, 44]))
