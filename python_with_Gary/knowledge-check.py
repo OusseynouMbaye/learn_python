5a08a458d9c9c1d9d5882baf7a4c9b067deac69c
Ouzinem <ouzinem@hotmail.com>
2021-12-19 22:17:21 -0500

    knowledg-check

--- python_with_Gary/knowledge-check.py
+++ python_with_Gary/knowledge-check.py
@@ -2,15 +2,15 @@
 # print(10 ** 3)
 
 url = "garyclarke.tech"
-# print(url[:-5])
+# print(url[:-5]) # remove .tech
 
 name = 'Ouzinem'
-phrase = 'Hello my name is'
-# print(f'{phrase} {name}')
+phrase = f'Hello my name is {name}'
+# print(phrase)
 
 my_list = ['Apple', 'Microsoft', 'Amazon', 'Google']
-removed_item = my_list.pop(2)
-# print(removed_item, my_list)
+removed_item = my_list.pop(2)  # we remove  Amazon
+# print(removed_item, my_list) # print Amazon ['Apple', 'Microsoft','Google']
 # for index, val in enumerate(my_list):
 #     print(index, val)
 
@@ -41,4 +41,8 @@ bools_one = 9 % 4 > 2 or 100 not in range(1, 100)
 bools_two = 1 + 3 ** 4 < 100 \
             and len(info['software']) not in {'a': 1, 'b': 2, 'c': 3}
 
-print(bools_two)
+# print(bools_two)
+liste = [3, 5, 7, 3, 1, 2, 3, 8, 0, 3]
+liste.remove(3, 3, 3, 3)
+
+print(liste)
