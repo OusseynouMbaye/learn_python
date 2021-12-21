546c1f5b11d2d17bac013ba2384bdfd85cd411d6
Ouzinem <ouzinem@hotmail.com>
2021-12-21 05:50:48 -0500

    intro how to import module

---
+++ python_with_Gary/how_to_import_modules/importing-modules.py
@@ -0,0 +1,19 @@
+# method 1:
+# import  math
+
+
+# method 2
+# from math import sqrt, floor
+from  random import choice
+
+
+# for method 1
+# print(math.sqrt(25))
+# print(math.ceil(2.1))
+
+# for method 2
+# print(sqrt(25))
+# print(floor(25.82))
+
+my_list = ['a', {2, 3, 4}, ('ouzin', 'azou', 'mbaye')]
+print(choice(my_list))
