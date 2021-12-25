cfb44f5bff8147010c4eb977c4e9fa251e306a87
Ouzinem <ouzinem@hotmail.com>
2021-12-25 11:23:52 -0500

    clear some indention

--- python_with_Gary/exercices/challenge1.py
+++ python_with_Gary/exercices/challenge1.py
@@ -1,13 +1,15 @@
-star_rating = [4,2,3,5,4,1,3,4,5,1]
+star_rating = [4, 2, 3, 5, 4, 1, 3, 4, 5, 1]
 number_input = int(input("Enter a nombre between 1 to 5 : "))
 number = number_input
-print("The number you enter is " , number)
+print("The number you enter is ", number)
 star_rating.append(number)
 print(star_rating)
-number_to_average =  star_rating[-5:]
+number_to_average = star_rating[-5:]
+
 
 def average_star(array_number):
-   return sum(array_number)/len(array_number)
+    return sum(array_number) / len(array_number)
+
 
 value_star = average_star(number_to_average)
 
