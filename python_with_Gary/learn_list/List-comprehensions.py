1fc752a055e25dc374fbaf2a1568d2aba0d91be6
Ouzinem <ouzinem@hotmail.com>
2021-12-28 13:57:47 -0500

    another method to append in list and learn while-else

---
+++ python_with_Gary/learn_list/List-comprehensions.py
@@ -0,0 +1,15 @@
+# my_list = []
+# for letter in 'word':
+#     my_list.append(letter)
+
+# another method to append in list
+# my_list = [letter for letter in 'word']
+# print(my_list)
+
+# my_list = []
+# for number in range(1, 11):
+#     print(my_list.append(number * 2))
+
+
+my_list = [number * 2 for number in range(1, 11)]
+print(my_list)
