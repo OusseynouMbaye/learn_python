894bd6ccfa695689281166aa5818a07104bee2f9
Ouzinem <ouzinem@hotmail.com>
2022-01-06 16:03:30 -0500

    clear some space

--- python_with_Gary/Packages/creating-module.py
+++ python_with_Gary/Packages/creating-module.py
@@ -5,6 +5,6 @@ tld = 'youtube.com'
 shortened = helpers.remove_tld(tld)
 numbers = 14235
 
-check_number = helpers.check_len(numbers,5)
+check_number = helpers.check_len(numbers, 5)
 
 print(check_number)
