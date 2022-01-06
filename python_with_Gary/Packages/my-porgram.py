7dc7be292aaebe857809eabb313788ec6b5b78c2
Ouzinem <ouzinem@hotmail.com>
2022-01-06 15:59:54 -0500

    how to create package

---
+++ python_with_Gary/Packages/my-porgram.py
@@ -0,0 +1,7 @@
+from MainPackage import main_functions
+from MainPackage.ChildPackage import child_functions
+# result = main_functions.products(10, 20)
+
+sphere_volume = child_functions.sphere(10)
+
+print(sphere_volume)
