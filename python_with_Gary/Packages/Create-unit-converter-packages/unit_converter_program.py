e6e865c117499d4a12e40b326e8ee489d25104ec
Ouzinem <ouzinem@hotmail.com>
2022-01-07 15:13:37 -0500

    starting project unit converter

---
+++ python_with_Gary/Packages/Create-unit-converter-packages/unit_converter_program.py
@@ -0,0 +1,24 @@
+from UnitConverterPackage import weights_and_measures
+
+# - Add functionality for selecting module category
+#      - create a module_options dictionary
+#      - build up the input message dynamically using values from module_options
+#      - get the user input
+#      - if the selected option is invalid, ask again
+
+module_options = {
+    '1': {'name': 'Weights and Measures', 'module': weights_and_measures}
+}
+category_choice_string = 'Please select a number from following categories : '
+
+for key, module_dict in module_options.items():
+    category_choice_string += f"\n[{key}] {module_dict['name']}"
+
+category_choice_string += '\n> '
+
+chosen_category = ''
+
+while chosen_category not in module_options.keys():
+    chosen_category = input(category_choice_string)
+
+print(chosen_category)
