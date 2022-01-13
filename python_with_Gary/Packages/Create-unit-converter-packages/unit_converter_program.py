206a4716b0ba1a1d32b75a655b3352bb8486f97a
Ouzinem <ouzinem@hotmail.com>
2022-01-12 21:10:38 -0500

    selecting conversion

--- python_with_Gary/Packages/Create-unit-converter-packages/unit_converter_program.py
+++ python_with_Gary/Packages/Create-unit-converter-packages/unit_converter_program.py
@@ -22,4 +22,16 @@ chosen_category = ''
 while chosen_category not in module_options.keys():
     chosen_category = input(category_choice_string)
 
-print(chosen_category)
+conversions = module_options[chosen_category]['module'].conversions
+
+conversions_choice_string = 'Please select a number from following options: '
+
+for key, conversion in conversions.items():
+    conversions_choice_string += f'\n[{key}] {conversion["name"]}'
+
+conversions_choice_string += '\n> '
+
+chosen_conversion = ''
+
+while chosen_conversion not in conversions.keys():
+    chosen_conversion = input(conversions_choice_string)
