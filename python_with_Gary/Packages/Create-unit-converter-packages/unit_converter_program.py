7a58643a7c396579e5d892c07cb249c9c72a214f
Ouzinem <ouzinem@hotmail.com>
2022-01-14 17:33:49 -0500

    create logic to show result when we call fontion

--- python_with_Gary/Packages/Create-unit-converter-packages/unit_converter_program.py
+++ python_with_Gary/Packages/Create-unit-converter-packages/unit_converter_program.py
@@ -1,4 +1,4 @@
-from UnitConverterPackage import weights_and_measures
+from UnitConverterPackage import weights_and_measures, distances_and_speeds
 
 # - PART II
 # - Add functionality for selecting module category
@@ -8,7 +8,8 @@ from UnitConverterPackage import weights_and_measures
 #      - if the selected option is invalid, ask again
 
 module_options = {
-    '1': {'name': 'Weights and Measures', 'module': weights_and_measures}
+    '1': {'name': 'Weights and Measures', 'module': weights_and_measures},
+    '2': {'name': 'Distances and speed', 'module': distances_and_speeds},
 }
 category_choice_string = 'Please select a number from following categories : '
 
@@ -35,3 +36,11 @@ chosen_conversion = ''
 
 while chosen_conversion not in conversions.keys():
     chosen_conversion = input(conversions_choice_string)
+
+converter = conversions[chosen_conversion]
+
+amount = float(input(f'Please enter an amount to convert from {converter["name"]}: '))
+
+result = converter['function'](amount, converter['inverse'])
+
+print(result)
